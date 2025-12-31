e28caf815326e9d7a9c391dabf3c1e815cdfbad7
Shareonniey <sharonadhiambo015@gmail.com>
2025-12-31 06:52:29 +0300

    adds to oop

--- lib/book.py
+++ lib/book.py
@@ -2,5 +2,22 @@
 
 class Book:
     pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+        pass
+    @property
+    def page_count(self):
+        return self._page_count
+    @page_count.setter
+    def page_count(self , value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print ("page_count must be an integer")
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
     
         
